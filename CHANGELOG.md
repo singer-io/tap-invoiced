156066362b0a04afb527c15c6aeb02b7314ef8ac
mukeshbhatt18gl <mukesh.bhatt@qlik.com>
2026-04-09 15:49:12 +0530

    update major version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
 # Changelog
 
-## 0.4.0 — 2026-04-01
+## 1.0.0
 * Upgrade `singer-python` to `6.8.0` [#11](https://github.com/singer-io/tap-invoiced/pull/11)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-invoiced",
-    version="0.4.0",
+    version="1.0.0",
     description="Singer.io tap for extracting data from Invoiced",
     author="Invoiced",
     url="https://invoiced.com",
